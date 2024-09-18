1c33a1aaf5fe6bb070dd0885e4957183663b219a
arthur <arthurfelipe5567@gmail.com>
2024-09-18 14:52:52 -0300

    feat: migration 3 subject_id column

--- src/main/resources/db/migration/V3__add_subject_to_activity.sql
+++ src/main/resources/db/migration/V3__add_subject_to_activity.sql
@@ -6,7 +6,7 @@ WHERE subject NOT IN (SELECT name FROM subject);
 ALTER TABLE activity ADD COLUMN subject_id INT;
 
 UPDATE activity a
-SET subject_id = s.id
+SET a.subject_id = s.id
 FROM subject s
 WHERE a.subject = s.name;
 
